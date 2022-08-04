615dc41aab4ff0cd751d9d6fb1947836bebfd417
Bojan Vrhovnik <bojan@vrhovnik.net>
2022-08-04 10:58:33 +0200

    fixing update and create to include user and to update workitem accordingly

--- src/TTASLN/TTA.SQL/WorkTaskRepository.cs
+++ src/TTASLN/TTA.SQL/WorkTaskRepository.cs
@@ -12,6 +12,53 @@ public class WorkTaskRepository : BaseRepository<WorkTask>, IWorkTaskRepository
     {
     }
 
+    public override async Task<WorkTask> InsertAsync(WorkTask entity)
+    {
+        await using var connection = new SqlConnection(connectionString);
+        var item = await connection.ExecuteScalarAsync(
+            $"INSERT INTO WorkTasks(Description,CategoryId,StartDate, EndDate,UserId,IsPublic)VALUES" +
+            $"(@{nameof(entity.Description)},@{nameof(entity.Category.CategoryId)},@{nameof(entity.Start)},@{nameof(entity.End)},@{nameof(entity.User.TTAUserId)},@{nameof(entity.IsPublic)});" +
+            "SELECT CAST(SCOPE_IDENTITY() as bigint)",
+            entity);
+
+        var workTaskId = Convert.ToInt64(item);
+        entity.WorkTaskId = workTaskId.ToString();
+
+        foreach (var tag in entity.Tags)
+        {
+            await connection.ExecuteAsync("INSERT INTO WorkTask2Tags(WorkTaskId,TagName)VALUES(@workTaskId,@tag)",
+                new { workTaskId, tag });
+        }
+
+        return entity;
+    }
+
+    public override async Task<bool> UpdateAsync(WorkTask entity)
+    {
+        await using var connection = new SqlConnection(connectionString);
+        var item = await connection.ExecuteAsync(
+            $"UPDATE WorkTasks SET Description=@{nameof(entity.Description)},CategoryId=@{nameof(entity.Description)},StartDate=@{nameof(entity.Start)}," +
+            $"EndDate=@{nameof(entity.End)},UserId=@{nameof(entity.User.TTAUserId)},IsPublic=@{nameof(entity.IsPublic)}) WHERE WorkTaskId=@{nameof(entity.WorkTaskId)}",
+            entity);
+
+        if (item < 0) return false;
+
+        string workTaskId = entity.WorkTaskId;
+        item = await connection.ExecuteAsync("DELETE FROM WorkTask2Tags WHERE WHERE WorkTaskId=@workTaskId",
+            new { workTaskId });
+
+        if (item > 0)
+        {
+            foreach (var tag in entity.Tags)
+            {
+                await connection.ExecuteAsync("INSERT INTO WorkTask2Tags(WorkTaskId,TagName)VALUES(@workTaskId,@tag)",
+                    new { workTaskId, tag });
+            }
+        }
+
+        return true;
+    }
+
     public async Task<PaginatedList<WorkTask>> WorkTasksForUserAsync(string userIdentificator,
         int pageIndex = 1, int pageSize = 10, string query = "")
     {
