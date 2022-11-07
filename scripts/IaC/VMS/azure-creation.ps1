8614cb713a6d452e9b307819e007a5167631a2bc
Bojan Vrhovnik <58401850+bovrhovn@users.noreply.github.com>
2022-11-07 22:07:07 +0100

    changed parameters to define containers workflow

--- scripts/IaC/VMS/azure-creation.ps1
+++ scripts/IaC/VMS/azure-creation.ps1
@@ -21,19 +21,21 @@ for ($index = 0; $index -lt $customers.count; $index++)
 {
     $currentRgName = "rg-$($customers[$index])"
     Write-Host "Current" $currentRgName
-    az deployment sub what-if --location $regionToDeploy --template-file rg.bicep --param resourceGroupName=$currentRgName
+    az deployment sub what-if --name $currentRgName --location $regionToDeploy --template-file rg.bicep --parameters resourceGroupName=$currentRgName
 }
 
-#deployment to production
+#deploy to production
 for ($index = 0; $index -lt $customers.count; $index++)
 {
     $currentRgName = "rg-$($customers[$index])"
     Write-Host "Current" $currentRgName
-    $data = az deployment sub create --location $regionToDeploy --template-file rg.bicep --param resourceGroupName=$currentRgName
-    $rgName = $data.properties.outputs.rgName.value
-    Write-Host $rgName "was created"
-    az deployment group what-if --resource-group $rgName --template-file vm.bicep --parameters vmName="$currentRgName-vm-$index" adminUsername="tta-admin" adminPassword="tta-P@ssw0rd1!"
-    az deployment group create --resource-group $rgName --template-file vm.bicep --parameters vmName="$currentRgName-vm-$index" adminUsername="tta-admin" adminPassword="tta-P@ssw0rd1!"
+    
+    az deployment sub create --name $currentRgName --location $regionToDeploy --template-file rg.bicep --parameters resourceGroupName=$currentRgName
+    Write-Host $currentRgName "was created"
+    $currentVMName = "vm-$($customers[$index])"
+    Write-Host "Current VM name " $currentVMName
+    az deployment group what-if --resource-group $currentRgName --template-file vm.bicep --parameters vmName="$currentRgName-vm-$index" windowsAdminUsername="tta-admin" windowsAdminPassword="tta-P@ssw0rd1!" vmName=$currentVMName
+    az deployment group create --resource-group $currentRgName --template-file vm.bicep --parameters vmName="$currentRgName-vm-$index" windowsAdminUsername="tta-admin" windowsAdminPassword="tta-P@ssw0rd1!" vmName=$currentVMName
 }
 Write-Host "Done with creating resources"
 
