2759fa907740e193e0127d9f1f83bb736a668915
Bojan Vrhovnik <58401850+bovrhovn@users.noreply.github.com>
2022-11-07 21:38:08 +0100

    adding script to generate resources and build / manage

--- scripts/IaC/Modernization/azure-creation.ps1
+++ scripts/IaC/Modernization/azure-creation.ps1
@@ -11,15 +11,20 @@
 # Version 0.4.6
 # SHORT CHANGE DESCRIPTION: adding import SQL option
 #>
-$rgName = "TTARG"
-Write-Host "Starting registry deploy in $rgName"
+param(
+    [string]$regionToDeploy = "WestEurope",
+    [string]$rgName = "rg-customer-ama-containers",   
+    [string]$containerappenv = "my-tta-environment",   
+    [string]$containerAppName = "tta-container-web"   
+)
+Write-Host "Starting registry deploy in $rgName in region $regionToDeploy"
 # deploy to azure group 
 $data = az deployment group create --resource-group $rgName --template-file registry.bicep --parameters registry.parameters.json | ConvertFrom-Json
 $loginName = $data.properties.outputs.loginName.value
-Write-Host "registry created, login aquired $loginName"
+Write-Host "Registry created, login aquired $loginName"
 
 # get sources - we can get it online if needed (download to local machine) - in devops pipeline you will prepare a step do download source code - for testing purposes you'll "simulate" the environet
-Set-Location "C:\Work\Projects\azure-monitor-automation-wth\"
+Set-Location "../../../../"
 
 Write-Host "Building images from provided source code"
 #build images and leverage ACR build engine to build the containers
@@ -30,7 +35,7 @@ az acr build --registry $loginName --image tta/statgen:1.0 -f 'containers/TTA.St
 Write-Host "Images built and prepped"
 
 # create SQL server, SQL database and deploy database
-Set-Location "C:\Work\Projects\azure-monitor-automation-wth\scripts\PWSH\03-Modernization"
+Set-Location "/scripts/IaC/Modernization/"
 Write-Host "Install Azure SQL"
 $currentServer = az deployment group create --resource-group $rgName --template-file sql.bicep --parameters sql.parameters.json | ConvertFrom-Json
 $server = $currentServer.properties.outputs.loginServer.value
@@ -60,37 +65,17 @@ Write-Host "Doing an import of bacpac file"
 # key is valid for 1 day - need to refresh it or call 
 # az storage blob generate-sas --account-name nameofstorageaccount -c ama -n TTADB.bacpac --permissions r --expiry 2022-01-01T00:00:00Z
 az sql db import -s $server -n $dbName --storage-key-type SharedAccessKey --storage-uri "https://webeudatastorage.blob.core.windows.net/ama/TTADB.bacpac" -g $rgName -p $password -u $username --storage-key "?sv=2021-04-10&st=2022-09-07T17%3A48%3A00Z&se=2022-09-09T17%3A48%3A47Z&sr=b&sp=r&sig=do3agVkOd8uQp4IJAj9HJNgGZg0HM8ZJX9%2B%2FMulqR2k%3D"
-
-#Write-Host "Restore database - executing script"
-#$commandToExecute = Get-Content "ttadb.sql"
-##Write-Host $commandToExecute
-#$conn = New-Object System.Data.SqlClient.SqlConnection
-#$conn.ConnectionString = $sqlConnection
-#$SqlCmd = New-Object System.Data.SqlClient.SqlCommand
-#$SqlCmd.CommandText = $commandToExecute
-#$SqlCmd.Connection = $conn
-## execute query
-#Write-Host "Opening connection to $dbName in $server"
-#$conn.Open()
-#$recordAffected=$SqlCmd.ExecuteNonQuery()
-#$conn.Close()
-#Write-Host "Database update successful, affected records $recordAffected"
-
-$location = "WestEurope"
-$containerappenv = "my-tta-environment"
-Write-Host "Create container app environment $containerappenv in $location "
-az containerapp env create --name $containerappenv --resource-group $rgName --location $location
-Write-Host "Environment $containerappenv created, going to create container app"
+Write-Host "Import done."
+Write-Host "Create container app environment $containerappenv in $regionToDeploy "
+az containerapp env create --name $containerappenv --resource-group $rgName --location $regionToDeploy
+Write-Host "Environment $containerappenv created, going to create container app to be used with containers"
 $registryServer = "$loginName.azurecr.io"
 $imageName = "$registryServer/tta/web:1.0"
 
 #get ACR pass
 $acrPass = az acr credential show -n $loginName --query passwords[0].value
-$containerAppName = "tta-container-web"
 Write-Host "Using $imageName to generate container app in environment $containerappenv"
 $fqdn = az containerapp create --max-replicas 3 --env-vars SqlOptions__ConnectionString=$sqlConnection --registry-server $registryServer --registry-username $loginName --registry-password $acrPass --name $containerAppName --resource-group $rgName --environment $containerappenv --image $imageName --target-port 80 --ingress 'external' --query properties.configuration.ingress.fqdn
 Write-Host "Container app running at $fqdn, starting app"
 
-#open website on with given URL - you can change below to whatever browser you'd like
-Start-Process "microsoft-edge:'$fqdn'"
  
--- scripts/IaC/VMS/azure-creation.ps1
+++ scripts/IaC/VMS/azure-creation.ps1
@@ -19,9 +19,9 @@ Write-Host "Creating resources in $regionToDeploy"
 #what if checks
 for ($index = 0; $index -lt $customers.count; $index++)
 {
-    $currentRgName = "rg-$( $customers[$index] )"
+    $currentRgName = "rg-$($customers[$index])"
     Write-Host "Current" $currentRgName
-    az deployment sub what-if --location $regionToDeploy --template-file rg.bicep --param resourceGroupName=$regionToDeploy
+    az deployment sub what-if --location $regionToDeploy --template-file rg.bicep --param resourceGroupName=$currentRgName
 }
 
 #deployment to production
@@ -31,8 +31,9 @@ for ($index = 0; $index -lt $customers.count; $index++)
     Write-Host "Current" $currentRgName
     $data = az deployment sub create --location $regionToDeploy --template-file rg.bicep --param resourceGroupName=$currentRgName
     $rgName = $data.properties.outputs.rgName.value
-    az deployment group what-if --resource-group $rgName --template-file vm.bicep --parameters vmName="$currentRgName-vm-$index)" adminUsername="tta-admin" adminPassword="tta-P@ssw0rd1!"
-    az deployment group create --resource-group $rgName --template-file vm.bicep --parameters vmName="$currentRgName-vm-$index)" adminUsername="tta-admin" adminPassword="tta-P@ssw0rd1!"
+    Write-Host $rgName "was created"
+    az deployment group what-if --resource-group $rgName --template-file vm.bicep --parameters vmName="$currentRgName-vm-$index" adminUsername="tta-admin" adminPassword="tta-P@ssw0rd1!"
+    az deployment group create --resource-group $rgName --template-file vm.bicep --parameters vmName="$currentRgName-vm-$index" adminUsername="tta-admin" adminPassword="tta-P@ssw0rd1!"
 }
 Write-Host "Done with creating resources"
 
