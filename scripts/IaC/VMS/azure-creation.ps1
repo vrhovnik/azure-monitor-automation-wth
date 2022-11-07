d0128d5bd7abc54959cb3450a9af4bd92e01252f
Bojan Vrhovnik <58401850+bovrhovn@users.noreply.github.com>
2022-11-07 20:58:01 +0100

    adding task for multiple VM's for customer

--- scripts/IaC/VMS/azure-creation.ps1
+++ scripts/IaC/VMS/azure-creation.ps1
@@ -10,31 +10,35 @@
 # GitHub      : https://github.com/vrhovnik
 # Version 0.2.4 
 #>
-$regionToDeploy = "WestEurope"
-Write-Host "Creating resources in $regionToDeploy"
-$customers = @("cust-ama-2", "cust-ama-3")
+param(
+    [string]$regionToDeploy="WestEurope",
+    [string[]]$customers = @("cust-ama-2", "cust-ama-3")
+)
 
-#what if checks
-for ($index = 0; $index -lt $customers.count; $index++)
-{ 
-    $currentRgName = "rg-$( $customers[$index] )"
-    Write-Host "Current" $currentRgName
-    az deployment sub what-if --location $regionToDeploy --template-file rg.bicep --param resourceGroupName=$currentRgName         
+Measure-Command {
+    Write-Host "Creating resources in $regionToDeploy"
+    #what if checks
+    for ($index = 0; $index -lt $customers.count; $index++)
+    {
+        $currentRgName = "rg-$( $customers[$index] )"
+        Write-Host "Current" $currentRgName
+        az deployment sub what-if --location $regionToDeploy --template-file rg.bicep --param resourceGroupName = $currentRgName
+    }
 }
-
 #deployment
-for ($index = 0; $index -lt $customers.count; $index++)
-{    
-    $currentRgName = "rg-$($customers[$index])"
-    Write-Host "Current" $currentRgName
-    $data=az deployment sub create --location $regionToDeploy --template-file rg.bicep --param resourceGroupName=$currentRgName
-    $rgName=$data.properties.outputs.rgName.value
-    az deployment group what-if --resource-group $rgName --template-file vm.bicep --parameters vmName="$currentRgName-vm-$index)" adminUsername="tta-admin" adminPassword="tta-P@ssw0rd1!"
-    az deployment group create --resource-group $rgName --template-file vm.bicep --parameters vmName="$currentRgName-vm-$index)" adminUsername="tta-admin" adminPassword="tta-P@ssw0rd1!"
+Measure-Command {
+    for ($index = 0; $index -lt $customers.count; $index++)
+    {
+        $currentRgName = "rg-$( $customers[$index] )"
+        Write-Host "Current" $currentRgName
+        $data = az deployment sub create --location $regionToDeploy --template-file rg.bicep --param resourceGroupName = $currentRgName
+        $rgName = $data.properties.outputs.rgName.value
+        az deployment group what-if --resource-group $rgName --template-file vm.bicep --parameters vmName = "$currentRgName-vm-$index)" adminUsername = "tta-admin" adminPassword = "tta-P@ssw0rd1!"
+        az deployment group create --resource-group $rgName --template-file vm.bicep --parameters vmName = "$currentRgName-vm-$index)" adminUsername = "tta-admin" adminPassword = "tta-P@ssw0rd1!"
+    }
 }
 
-#$rgName=$data.properties.outputs.rgName.value
-#Write-Host "Resource group $rgName has been created, creating VM"
-#az deployment group create --resource-group $rgName --template-file vm.bicep --parameters  
+Write-Host "Done with creating resources"
+
 
 
