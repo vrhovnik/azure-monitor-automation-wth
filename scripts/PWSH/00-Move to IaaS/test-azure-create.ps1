9cd2567cb8a021d4b60798434a928b489f7fabdb
Bojan Vrhovnik <58401850+bovrhovn@users.noreply.github.com>
2022-11-07 13:12:56 +0100

    adding basic IaC instructions to create 1 customer

--- scripts/PWSH/00-Move to IaaS/test-azure-create.ps1
+++ scripts/PWSH/00-Move to IaaS/test-azure-create.ps1
@@ -4,5 +4,5 @@ $regionToDeploy="WestEurope"
 $rgName="TTARG"
 # use what-if to check what will happen to resource group
 # https://docs.microsoft.com/en-us/cli/azure/deployment/group?view=azure-cli-latest#az-deployment-group-what-if
-az deployment sub what-if --location $regionToDeploy --template-file rg.bicep --parameters rg.parameters.json
+az deployment sub what-if --location $regionToDeploy --template-file rg.bicep --parameters rg.parameters.json         
 az deployment group what-if --resource-group $rgName --template-file vm.bicep --parameters vm.parameters.json
